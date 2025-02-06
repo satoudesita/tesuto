ae744a138a7566a06231043a90e4c68ee958cf64
satoudesita <167180203+satoudesita@users.noreply.github.com>
2025-02-06 09:02:09 +0000

    認証番号完成

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,9 @@ from PIL import Image
 import pytz
 import sqlite3
 import hashlib
-import threading
 import random
 
+# POSTリクエストを送信する関数
 def send_post_request(url, data):
     try:
         response = requests.post(url, json=data)
@@ -22,154 +22,99 @@ def send_post_request(url, data):
     except Exception as e:
         st.write(f"リクエストエラー: {e}")
 
+# QRコードを生成する関数
+def generate_qrcode(data):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(data)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill='black', back_color='white')
+
+    # QRコードをバイナリデータとして返す
+    img_byte_arr = BytesIO()
+    img.save(img_byte_arr, format='PNG')
+    img_byte_arr.seek(0)
+
+    return img_byte_arr.getvalue()  # バイナリデータを返す
+
+# バーコードを生成する関数
+def generate_code128_barcode(data):
+    barcode_class = barcode.get_barcode_class('code128')
+    code128 = barcode_class(data, writer=ImageWriter())
+
+    # バーコードをバイナリデータとして生成
+    buffer = BytesIO()
+    code128.write(buffer)
+    buffer.seek(0)
+
+    return buffer.getvalue()  # バイナリデータを返す
+
 # タブを作成
-tab1, tab2 ,tab3,tab4,tab5= st.tabs(["QR", "BR","test","遅刻証明","遅刻証明読み取り"])
+tab1, tab2 ,tab3,tab4,tab5 = st.tabs(["QR", "BR", "test", "遅刻証明", "遅刻証明読み取り"])
 
 with tab1:
-
-    # QRコードを生成する関数
-    def generate_qrcode(data):
-        qr = qrcode.QRCode(
-            version=1,
-            error_correction=qrcode.constants.ERROR_CORRECT_L,
-            box_size=10,
-            border=4,
-        )
-        qr.add_data(data)
-        qr.make(fit=True)
-
-        img = qr.make_image(fill='black', back_color='white')
-
-        # PIL Imageをバイナリストリームに変換
-        img_byte_arr = BytesIO()
-        img.save(img_byte_arr, format='PNG')
-        img_byte_arr.seek(0)
-
-        return img_byte_arr
-
-    # Code128バーコードを生成する関数
-    def generate_code128_barcode(data):
-        # Code128はASCIIのみサポート
-        barcode_class = barcode.get_barcode_class('code128')
-        code128 = barcode_class(data, writer=ImageWriter())
-
-        # バーコードをバイナリストリームとして生成
-        buffer = BytesIO()
-        code128.write(buffer)
-        buffer.seek(0)
-
-        return buffer
-
-    # Streamlit UI部分
+    # ユーザー入力の取得
     st.write("以下のフォームに情報を入力してください。")
-    st.write("QRコードは日本語対応可バーコードは日本語対応不可")
-
-    # ユーザーが追加する情報の入力項目
     fields = ["high school", "glade", "class", "name"]
-
-    # 初期の入力項目を表示
     user_input = {field: st.text_input(field) for field in fields}
-
-    # 追加情報の項目をテキストボックスで動的に入力
+    
     st.subheader("add information")
     additional_fields = []
     add_field = st.text_input("the name")
     add_value = st.text_input("the value")
-
     if add_field and add_value:
         additional_fields.append((add_field, add_value))
-
-    # 入力フォームがすべて埋まった場合
+    
+    # 入力が全て埋まった場合にQRコードを生成
     if all(user_input[field] for field in fields):
-        # 名前、電話番号、住所を1つの文字列にまとめる
         combined_data = "\n".join([f"{key}: {value}" for key, value in user_input.items()])
-
-        # 追加された情報を組み合わせ
         for field_name, field_value in additional_fields:
             combined_data += f"\n{field_name}: {field_value}"
+        
+        qr_img_data = generate_qrcode(combined_data)
+        data = {"combined_data": combined_data, "qr_image": qr_img_data}
+        
+        # APIに送信
+        send_post_request('API_URL', data)
+        st.image(qr_img_data, caption="生成されたQRコード", use_container_width=True)
 
-        # QRコードを生成
-        st.subheader("生成されたQRコード")
-        qr_img = generate_qrcode(combined_data)
-        st.image(qr_img, caption="QRコード", use_container_width=True)
-
+    # バーコード生成
     with st.expander("バーコード"):
-        # ユーザーからの入力を受け取る
         text_input = st.text_input("バーコードにするテキストを入力してください。:")
-
-        # 入力されたテキストでCODE39バーコードを生成
         if text_input:
-            try:
-                # CODE39バーコードを作成
-                code39 = barcode.get_barcode_class('code39')
-
-                # ImageWriterのオプションを設定
-                writer = ImageWriter()
-                writer_options = {
-                    'module_width': 0.1,  # バーコードのモジュール幅
-                    'module_height': 3   # バーコードの高さ
-                }
-
-                # チェックサムなしのバーコードを作成
-                barcode_img = code39(text_input, writer=writer)
-                barcode_img = barcode_img.__class__(text_input, writer=writer, add_checksum=False)
-
-                # バーコード画像をメモリ上で保存
-                buf = BytesIO()
-                barcode_img.write(buf, writer_options)
-                buf.seek(0)
-
-                # バーコード画像をStreamlitに表示
-                st.image(buf, caption="生成されたCODE39バーコード", use_container_width=True)
-
-            except Exception as e:
-                st.error(f"バーコード生成に失敗しました: {e}")
+            barcode_data = generate_code128_barcode(text_input)
+            st.image(barcode_data, caption="生成されたCODE128バーコード", use_container_width=True)
 
 with tab2:
-
     with st.form(key='my_form', clear_on_submit=True):
         st.text('テキストボックスの内容と時刻を取得')            
         user_input = st.text_input(label='テキストを入力してください:', key='input')
-
-        
         submit_button = st.form_submit_button(label='送信')
 
     if submit_button and user_input:
-        # 日本のタイムゾーンを指定
         japan_tz = pytz.timezone('Asia/Tokyo')
-        # 現在の日本時刻を取得
         current_time_japan = datetime.now(japan_tz)
-        current_time=current_time_japan.strftime('%m-%d %H:%M')
-        # 日本時刻を表示
+        current_time = current_time_japan.strftime('%m-%d %H:%M')
+        
         st.text(f"入力されたテキスト: {user_input}")
         st.text(f"入力時刻: {current_time}")
-        data = {
-            "名前": user_input,
-            "時間": current_time,
-        }
+        data = {"名前": user_input, "時間": current_time}
         send_post_request('https://prod-08.japaneast.logic.azure.com:443/workflows/2dad7268f2844042bae005c2ec7916f6/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=V-60f4bGMzshRcghrvSV7qt-WEgKqbgQGfGk2F8BQPk', data)
+
 with tab3:
     API_URL = "https://api.jancodelookup.com/"
-    API_ID = "96385e12558d53c366efb3c187ef0440" 
-
+    API_ID = "96385e12558d53c366efb3c187ef0440"
+    
     def search_product_by_code(jan_code):
-
-        params = {
-            'appId': API_ID,     # アプリID（
-            'query': jan_code,    # JANコード
-            'hits': 1,            # 取得件数（1件だけ取得）
-            'page': 1,            # 1ページ目を指定
-            'type': 'code',       # コード番号検索
-        }
-        
-        # APIにリクエストを送信
+        params = {'appId': API_ID, 'query': jan_code, 'hits': 1, 'page': 1, 'type': 'code'}
         response = requests.get(API_URL, params=params)
-        
-        # レスポンスのステータスコードを確認
         if response.status_code == 200:
-            data = response.json()  # JSONレスポンスをパース
-            
-        
+            data = response.json()
             if 'product' in data and len(data['product']) > 0:
                 product = data['product'][0]
                 return product
@@ -179,17 +124,13 @@ with tab3:
         else:
             st.error(f"APIリクエストに失敗しました: {response.status_code} - {response.text}")
             return None
-
+    
     st.subheader("JANコードで商品検索")
     with st.form(key='my2_form', clear_on_submit=True):
-        jan_code = st.text_input(label="JANコードを入力してください",key="search")           
-        
+        jan_code = st.text_input(label="JANコードを入力してください", key="search")           
         submit_button = st.form_submit_button(label='送信')
 
-    
-
     if jan_code:
-        # 商品を検索
         product = search_product_by_code(jan_code)
         if product:
             st.text("商品情報:")
@@ -197,22 +138,28 @@ with tab3:
             st.text(f"ブランド名: {product.get('brandName', '不明')}")
             st.text(f"メーカー名: {product.get('makerName', '不明')}")
             st.text(f"詳細ページ: [商品ページ](https://www.jancodelookup.com/code/{product['codeNumber']})")
-            st.image(product.get('itemImageUrl')) 
-    else:
-        st.text("JANコードを入力してください")
+            st.image(product.get('itemImageUrl'))
+
 with tab4:
 
-    def send_post_request(url1, data):
+    # POSTリクエストを送信する関数
+    def send_post_request(url, data):
         try:
-            response = requests.post(url1, json=data)
+            # 'body'に文字列形式で送信
+            request_data = {
+                "body": str(data)  # データを文字列として 'body' フィールドに格納
+            }
+            
+            response = requests.post(url, json=request_data)  # json形式でPOSTリクエストを送信
             if response.status_code == 200:
                 st.write("成功: ", response.json())
             else:
                 st.write("エラー: ", response.status_code)
+                st.write("エラーメッセージ: ", response.text)  # エラーメッセージを表示
         except Exception as e:
             st.write(f"リクエストエラー: {e}")
 
-    # SQLiteデータベースを初期化する関数
+    # ユーザーのDB初期化
     def init_db():
         conn = sqlite3.connect("users.db")
         cursor = conn.cursor()
@@ -226,11 +173,11 @@ with tab4:
         conn.commit()
         conn.close()
 
-    # パスワードをハッシュ化する関数
+    # パスワードをハッシュ化
     def hash_password(password):
         return hashlib.sha256(password.encode('utf-8')).hexdigest()
 
-    # ユーザー情報をデータベースに保存する関数
+    # ユーザーをDBに保存
     def save_user(username, password):
         conn = sqlite3.connect("users.db")
         cursor = conn.cursor()
@@ -245,7 +192,7 @@ with tab4:
             st.error("このユーザー名はすでに存在します")
         conn.close()
 
-    # ユーザー名とパスワードで認証する関数
+    # ユーザー認証
     def authenticate_user(username, password):
         conn = sqlite3.connect("users.db")
         cursor = conn.cursor()
@@ -257,80 +204,69 @@ with tab4:
         conn.close()
         return user
 
-    # QRコードを生成する関数
-    def generate_qr(data):
-        qr = qrcode.make(data)
-        qr_path = "qr_code.png"
-        qr.save(qr_path)
-        return qr_path
- 
-    # Streamlit UI部分
+    # 初期化
     init_db()
 
-    # ログイン状態を追跡
     if 'logged_in' not in st.session_state:
         st.session_state.logged_in = False
         st.session_state.username = ""
 
-    # ログインした場合の処理
     if st.session_state.logged_in:
         selected_option2 = st.radio("選択肢", ["遅刻証明", "ログアウト"])
         if selected_option2 == "遅刻証明":
             st.text(f"{st.session_state.username} ログイン")
             st.subheader("遅刻証明書発行")
-            reason = st.text_input(label="reason",key="why")
+            reason = st.text_input(label="理由", key="why")
             late_button = st.button(label="発行", key="hakkou")
             if late_button:
-                # 日本のタイムゾーンを指定
+                # 現在時刻を取得
                 japan_time = pytz.timezone('Asia/Tokyo')
-                # 現在の日本時刻を取得
                 get_time_japan = datetime.now(japan_time)
-                get_time = get_time_japan.strftime('%m-%d %H:%M')
-                # 日本時刻を表示
-                st.text(f"名前: {st.session_state.username}")
-                st.text(f"入力時刻: {get_time}")
-                st.text(f"理由:{reason}")
-                st.subheader("認証コード")
-                ranndamu=random.randint(100,999)
-                ninnsyou=st.subheader(f"{st.session_state.username}{ranndamu}")
-                data1 = {
-                    "名前": st.session_state.username,
-                    "時間": get_time,
-                    "理由": reason
-                }
+                get_time = get_time_japan.strftime('%m-%d %H:%M ')
+                get_day = get_time_japan.strftime('%A')
+                
+                # 遅刻証明の情報を表示
+                st.subheader(f"名前: {st.session_state.username}")
+                st.subheader(f"入力時刻: {get_time}({get_day})")
+                st.subheader(f"理由: {reason}")
+                
+                ranndamu = random.randint(100, 999)  # ランダムな3桁の数字
+                ranndamu1 = str(ranndamu)  # 文字列に変換
+
+                # 文字列として「1221+ranndamu1」を表示
+                tikokuninnsyou = "1221" + ranndamu1  # 結果を文字列として設定
+
+                # st.text() に渡す際は tikokuninnsyou をそのまま渡す
+                st.subheader(f"認証番号{tikokuninnsyou}")  # 正しい文字列を表示
+
+                # 遅刻証明書発行のために必要なデータ（ランダムな数字を追加）
+                data1 = {"名前": st.session_state.username, "時間": get_time, "理由": reason}
+                
+                # データ送信 (遅刻証明用)
                 send_post_request('https://prod-07.japaneast.logic.azure.com:443/workflows/e30f108c25324d62bfa50133e41c47bb/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=d3BzwwA54bqFHmhHwvCPZaXdScGUHJRS8pWwoXx-pds', data1)
+                
+                # ランダムな数字を別のURLに送信
+                send_post_request('https://prod-01.japaneast.logic.azure.com:443/workflows/38f7b8c8d476411d8d4351e0638c6750/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DQl_g5amg0IRCFIs1lRiIBvicQ1Z9JI9i7uNgWKKu2g', tikokuninnsyou)
 
-                data2={
-                    "認証コード":ninnsyou
-                }
-                send_post_request('https://prod-01.japaneast.logic.azure.com:443/workflows/38f7b8c8d476411d8d4351e0638c6750/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=DQl_g5amg0IRCFIs1lRiIBvicQ1Z9JI9i7uNgWKKu2g', data2)
-
-                # QRコードを生成
-                qr_path = generate_qr(data)
-
-                # QRコードを表示
+                # QRコードを生成して表示
+                qr_path = generate_qrcode(data1)  # `generate_qrcode` は独自関数なので、適切に定義が必要です。
                 st.image(qr_path, caption="遅刻証明書QRコード")
+                
         else:
-            # ログアウトボタン
             if st.button("ログアウト"):
                 st.session_state.logged_in = False
                 st.session_state.username = ""
                 st.success("ログアウトしました")
-
-
     else:
-        # サインアップ / ログインの選択肢
         st.subheader("ログイン / サインアップ")
         selected_option = st.radio("選択肢", ["サインアップ", "ログイン"])
 
-        # サインアップフォーム
         if selected_option == "サインアップ":
             signup_username = st.text_input("ユーザー名")
             signup_password = st.text_input("パスワード", type="password")
             signup_confirm_password = st.text_input("パスワード確認", type="password")
-            
             signup_button = st.button("サインアップ")
-            
+
             if signup_button:
                 if signup_password == signup_confirm_password:
                     save_user(signup_username, signup_password)
@@ -338,13 +274,11 @@ with tab4:
                 else:
                     st.error("パスワードが一致しません。")
 
-        # ログインフォーム
         if selected_option == "ログイン":
             login_username = st.text_input("ユーザー名", key="login_username")
             login_password = st.text_input("パスワード", type="password", key="login_password")
-            
             login_button = st.button("ログイン")
-            
+
             if login_button:
                 user = authenticate_user(login_username, login_password)
                 if user:
